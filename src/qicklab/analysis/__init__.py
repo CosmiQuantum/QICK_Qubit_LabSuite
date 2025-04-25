acef009a1b1e2d623dc3dc09d4a36a7f745a7a53
Dylan Temples <dtemps123@gmail.com>
2025-04-25 13:09:38 -0500

    alphabetize, import auto thresh demo:

--- src/qicklab/analysis/__init__.py
+++ src/qicklab/analysis/__init__.py
@@ -1,5 +1,3 @@
-# from .analysis_helper_TLS_comprehensive import *
-
 from .fit_functions import *
 from .fitting import *
 from .plotting import *
@@ -7,9 +5,9 @@ from .plotting import *
 from .h5_data import *
 
 ## Specific analysis classes go here
+from .auto_threshold import auto_threshold, auto_threshold_demo
 from .qspec import qspec
-from .t1 import t1, t1_demo
-from .ssf import ssf
 from .resstarkspec import resstarkspec
+from .ssf import ssf
 from .starkspec import starkspec
-from .auto_threshold import auto_threshold
+from .t1 import t1, t1_demo

4310ce57116cae0d67b58877b012d2bf23ea5adc
Dylan Temples <dtemps123@gmail.com>
2025-05-07 18:49:50 -0500

    add classes for aggregate analysis

---
+++ src/qicklab/analysis/AnalysisClass.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+from ..datahandling.datafile_tools import find_h5_files, load_h5_data, get_data_field
+
+
+class AnalysisClass:
+
+    self.required_ana_keys = []
+    self.optional_ana_keys = []
+
+
+    def __init__(self, data_dir, dataset, qubit_index, folder="study_data", expt_name="qspec_ge", ana_params={}):
+        
+        ## Save the arguments to the class
+        self.data_dir = data_dir
+        self.dataset = dataset
+        self.qubit_index = qubit_index
+        self.expt_name = expt_name
+        self.folder = folder
+
+        ## Check the analysis params keys against the required keys
+        for req_key in self.required_ana_keys:
+            if req_key not in ana_params.keys(): 
+                raise KeyError("ERROR: required keys not found in ana_params: "+",".join(self.required_ana_keys))
+
+
+    def load_all(self, verbose=False):
+        ## Create a container for the output
+        analysis_data = {}
+
+        ## Find all the H5 files for this dataset
+        h5_files, data_path, n = find_h5_files(self.data_dir, self.dataset, self.expt_name, folder=self.folder)
+
+        ## For each file in the dataset, load the data, pull specific data fields,
+        ## and save it to the output dictonary.
+        for i,h5_file in enumerate(h5_files):
+            load_data = load_h5_data(os.path.join(data_path, h5_file), 'QSpec')
+            timestamps = get_data_field(load_data, 'QSpec', self.qubit_index, 'Dates')
+            
+            ## Save a data dictionary to the output dictionary, one key for each piece of data
+            ## that will be needed later to perform the analysis
+            analysis_data[h5_file] = {
+                "timestamps": timestamps,
+            }
+
+        ## Save the output dictionary to the class instance and then return it
+        self.analysis_data = analysis_data
+        return analysis_data
+
+
+    def run_analysis(self, verbose=False):
+        ## Create a container for the output
+        analysis_result = {}
+
+        ## For each file in the dataset, use the information saved in self.analysis data to
+        ## do something, and save it to the output dictonary.
+        for k in self.analysis_data.keys():
+            print(self.analysis_data[k]["timestamps"][0])
+
+            analysis_result[k] = {
+                "result":None,
+            }
+
+        ## Save the output dictionary to the class instance and then return it
+        self.analysis_result = analysis_result
+        return analysis_result
+
+
+    def cleanup(self, clear_results=False):
+        del self.analysis_data
+        if clear_results: del self.analysis_result
+
--- src/qicklab/datahandling/DataAggregator.py
+++ src/qicklab/datahandling/DataAggregator.py
@@ -1,8 +1,9 @@
-import os, glob
+import os
 import time
 import numpy as np
 
 from datetime import datetime
+
 from .datafile_tools import DATETIME_FMT
 
 class DataAggregator:
@@ -15,7 +16,7 @@ class DataAggregator:
         self.mintimestamp = time.mktime(datetime(2000,1,1,0,0,0).timetuple()) if self.mindataset is None else time.mktime(datetime.strptime(self.mindataset, DATETIME_FMT).timetuple())
         self.maxtimestamp = time.mktime(datetime(2100,1,1,0,0,0).timetuple()) if self.maxdataset is None else time.mktime(datetime.strptime(self.maxdataset, DATETIME_FMT).timetuple())
 
-    def load_all(self, verbose=False):
+    def find_all_datasets(self, verbose=False):
         alldata = {}
 
         ## Ensure the substudy_list is a list even if only a sigle substudy was provided
@@ -51,7 +52,57 @@ class DataAggregator:
         goodruns = np.array([run[0] for run in runlist[run_idx]])
         goodpaths = np.array([path[0] for path in pathlist[run_idx]])
 
+        ## Save the good runs and their respective paths
+        self.datasets = goodruns
+        self.datapaths = goodpaths
+        self.ndatasets = len(goodruns)
         return goodruns, goodpaths 
+
+    def run_analysis(AnalysisClass, qubit_index, analysis_params={}, datasets=None, datapaths=None, verbose=False):
+
+        ## Check the datasets and paths
+        if (datasets is not None) and (datapaths is not None):
+            if len(datasets) != len(datapaths):
+                raise Exception("ERROR: provided datasets and datapaths are different lenghts.")
+        else: ## At least one is None
+            datasets = self.datasets
+            datapaths = self.datapaths
+            ndatasets = self.ndatasets
+
+        ## Confirm the presence of required keys in analysis parameters
+        required_keys = ["name"]
+        optional_keys = ["folder"]
+        for req_key in required_keys:
+            if req_key not in analysis_params.keys(): 
+                raise KeyError("ERROR: required keys not found in analysis_params: "+",".join(required_keys))
+
+        ## Create a container for the results, one key per dataset
+        ana_result = { dataset:{} for dataset in datasets}
+
+        ## Now for each dataset, run the analysis
+        for i,(dataset,datapath) in enumerate(zip(datasets,datapaths)):
+
+            ## Now instantiate an analysis instance for this dataset
+            anaClass = AnalysisClass(datapath, dataset, qubit_index, 
+                folder = "study_data" if ("folder" not in analysis_params.keys()) else analysis_params["folder"], 
+                expt_name = analysis_params["name"], 
+                ana_params = analysis_params )
+
+            ## Load the required data for this analysis, and run it
+            _ = anaClass.load_all() 
+
+            ana_result[dataset] = anaClass.run_analysis(verbose=verbose)
+
+            ## Free some memory
+            anaClass.cleanup(clear_results=True)
+            del anaClass
+
+        self.result = ana_result
+        return ana_result
+
+        
+
+
 
 
 

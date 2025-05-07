0e93a105bcbd1c6b783069bc8c26ad700f76b7cb
Dylan Temples <dtemps123@gmail.com>
2025-05-07 14:15:44 -0500

    use new data loading methods

--- src/qicklab/analysis/qspec.py
+++ src/qicklab/analysis/qspec.py
@@ -27,14 +27,21 @@ class qspec:
         Q = []
 
         load_data = load_h5_data(os.path.join(data_path, h5_files[0]), 'QSpec', save_r=1)
-        qspec_probe_freqs = process_h5_data(load_data['QSpec'][self.QubitIndex].get('Frequencies', [])[0][0].decode())
 
-        for h5_file in h5_files:
+        for i,h5_file in enumerate(h5_files):
+
             load_data = load_h5_data(os.path.join(data_path, h5_file), 'QSpec', save_r=1)
-            dates.append(datetime.datetime.fromtimestamp(load_data['QSpec'][self.QubitIndex].get('Dates', [])[0][0]))
+            if i==0: qspec_probe_freqs = get_data_field(load_data, 'QSpec', self.QubitIndex, 'Frequencies')
 
-            I.append(np.array(process_h5_data(load_data['QSpec'][self.QubitIndex].get('I', [])[0][0].decode())))
-            Q.append(np.array(process_h5_data(load_data['QSpec'][self.QubitIndex].get('Q', [])[0][0].decode())))
+            timestamps = get_data_field(load_data, 'QSpec', self.QubitIndex, 'Dates')
+            dates.append(datetime.datetime.fromtimestamp(timestamps))
+            I.append(get_data_field(load_data, 'QSpec', self.QubitIndex, 'I'))
+            Q.append(get_data_field(load_data, 'QSpec', self.QubitIndex, 'Q'))
+
+            # dates.append(datetime.datetime.fromtimestamp(load_data['QSpec'][self.QubitIndex].get('Dates', [])[0][0]))
+
+            # I.append(np.array(process_h5_data(load_data['QSpec'][self.QubitIndex].get('I', [])[0][0].decode())))
+            # Q.append(np.array(process_h5_data(load_data['QSpec'][self.QubitIndex].get('Q', [])[0][0].decode())))
 
         return dates, n, qspec_probe_freqs, I, Q
 
